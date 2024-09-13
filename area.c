7fd93ab2295eef7a3b1bc32f2c4d903307198fa0
fredluxy <otienofredodhiambo@gmail.com>
2024-09-13 12:50:45 +0300

    Cprograms

---
+++ area.c
@@ -0,0 +1,11 @@
+//finding area of atriangle 
+#include<stdio.h>
+
+int main()
+{
+    int area,base,height;
+    base=24;
+    height=26;
+    area=(height*base)/2;
+    printf("area of triangle is %d",area);
+}
